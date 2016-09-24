79952fe5f2e263b5b1e47977532a41f0da97c20c
drzzm32 <drzzm32@gmail.com>
2016-09-24 20:24:55 +0800

    update keypad code

--- STC15/main.c
+++ STC15/main.c
@@ -9,7 +9,19 @@ void main() {
 	
 	while (1) {
 		tmp = (char)scanKey();
-		if (tmp != 0xFF) {
+		if (tmp == KeyCL) {
+			while (tmp == KeyCL)
+				tmp = (char)scanKey();
+			CapsLock = ~CapsLock;
+			CLPOS = CapsLock;
+		} else if (tmp == KeyCt) {
+		
+		} else if (tmp == KeyAl) {
+		
+		} else if (tmp == KeyEsc) {
+		
+		} else if (tmp != 0xFF) {
+			if (tmp >= 'A' && tmp <= 'Z' && !CapsLock) tmp += 32;
 			send(tmp);
 			tmp = 0xFF;
 			delay(10);
